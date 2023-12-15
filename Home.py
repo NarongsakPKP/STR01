de0b9359d667b199370b0b31efcdf206b55a0552
NarongsakPKP <glofkama@gmail.com>
2023-12-15 14:22:00 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.header("Narongsak Pankerdpon")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ณรงค์ศักดิ์ ปานเกิดผล')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
